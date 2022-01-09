1b2e1b713cafd49befea00fd3ee6fde313215165
sunclx <sunclx@gmail.com>
2022-01-09 21:41:49 +0800

    fix

--- src/deno/convert.js
+++ src/deno/convert.js
@@ -18,7 +18,7 @@ if (import.meta.main) {
   // 输出路径
   const output_path = "./output.yaml";
   // 规则策略
-  const config_path = "src/clash.yaml";
+  const config_path = "src/deno/clash.yaml";
 
   if (sub_url === "") {
     Deno.exit();
