0b24c6cc27ee82e6db69a55de39077daf6493402
sunclx <sunclx@gmail.com>
2022-03-28 21:22:36 +0800

    update main.js

--- src/deno/main.js
+++ src/deno/main.js
@@ -3,8 +3,10 @@ const resp = await fetch(
 );
 const data = await resp.text();
 const time = new Date();
-const filename = `./archives/jeidian4.18/${
-  time.toISOString().substring(0, 10)
-}.txt`;
+
+// const filename = `./archives/jeidian4.18/${
+//   time.toISOString().substring(0, 10)
+// }.txt`;
+const filename = `./archives/jeidian4.18/latest.txt`;
 const decoded_data = atob(data, "base64");
 await Deno.writeTextFile(filename, decoded_data);
